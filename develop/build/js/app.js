3f6558e35fdbcffceaf18ae3c13e39782570431c
TakashiSato <takashisato.235711@gmail.com>
2016-11-06 12:23:01 +0900

    loadに修正

--- develop/build/js/app.js
+++ develop/build/js/app.js
@@ -63,35 +63,37 @@
 	window.onload = function () {
 	  var $rootApp = (0, _jquery2.default)('#rootApp');
 
-	  _jquery2.default.ajax({
-	    type: 'GET',
-	    url: './html/partial/_svg_sprite_test.html',
-	    dataType: 'html',
-	    async: false
-	  }).done(function (data) {
-	    $rootApp.html(data);
+	  $rootApp.load('./html/partial/_svg_sprite_test.html');
 
-	    new _jquery2.default.Deferred().resolve().promise().then(function () {
-	      var rootName = $rootApp.find('#rootSub').attr('data-root-name');
-
-	      switch (rootName) {
-	        case 'validationTest':
-	          var listEach = [].forEach.bind(_alias2.default.getTag('input'));
-	          listEach(function (item) {
-	            new _validation2.default(item);
-	          });
-	          // [].forEach().apply(Al.getTag('input'));
-
-	          break;
-	        case 'spriteTest':
-	          break;
-	        case 'svgSpriteTest':
-	          break;
-	        default:
-	          break;
-	      }
-	    });
-	  });
+	  // $.ajax({
+	  //   type    : 'GET',
+	  //   url     : './html/partial/_svg_sprite_test.html',
+	  //   dataType: 'html',
+	  //   async   : false
+	  // }).done((data) => {
+	  //   $rootApp.html(data);
+	  //
+	  //   new $.Deferred().resolve().promise().then(() => {
+	  //     const rootName = $rootApp.find('#rootSub').attr('data-root-name');
+	  //
+	  //     switch (rootName) {
+	  //       case 'validationTest':
+	  //         const listEach = [].forEach.bind(Al.getTag('input'));
+	  //         listEach(item => {
+	  //           new Valid(item);
+	  //         });
+	  //         // [].forEach().apply(Al.getTag('input'));
+	  //
+	  //         break;
+	  //       case 'spriteTest':
+	  //         break;
+	  //       case 'svgSpriteTest':
+	  //         break;
+	  //       default:
+	  //         break;
+	  //     }
+	  //   })
+	  // });
 	};
 
 /***/ },
--- develop/src/js/app.js
+++ develop/src/js/app.js
@@ -5,33 +5,35 @@ import $     from '../../../bower_components/jquery/dist/jquery.min';
 window.onload = () => {
   const $rootApp  = $('#rootApp');
 
-  $.ajax({
-    type    : 'GET',
-    url     : './html/partial/_svg_sprite_test.html',
-    dataType: 'html',
-    async   : false
-  }).done((data) => {
-    $rootApp.html(data);
+  $rootApp.load('./html/partial/_svg_sprite_test.html');
 
-    new $.Deferred().resolve().promise().then(() => {
-      const rootName = $rootApp.find('#rootSub').attr('data-root-name');
-
-      switch (rootName) {
-        case 'validationTest':
-          const listEach = [].forEach.bind(Al.getTag('input'));
-          listEach(item => {
-            new Valid(item);
-          });
-          // [].forEach().apply(Al.getTag('input'));
-
-          break;
-        case 'spriteTest':
-          break;
-        case 'svgSpriteTest':
-          break;
-        default:
-          break;
-      }
-    })
-  });
+  // $.ajax({
+  //   type    : 'GET',
+  //   url     : './html/partial/_svg_sprite_test.html',
+  //   dataType: 'html',
+  //   async   : false
+  // }).done((data) => {
+  //   $rootApp.html(data);
+  //
+  //   new $.Deferred().resolve().promise().then(() => {
+  //     const rootName = $rootApp.find('#rootSub').attr('data-root-name');
+  //
+  //     switch (rootName) {
+  //       case 'validationTest':
+  //         const listEach = [].forEach.bind(Al.getTag('input'));
+  //         listEach(item => {
+  //           new Valid(item);
+  //         });
+  //         // [].forEach().apply(Al.getTag('input'));
+  //
+  //         break;
+  //       case 'spriteTest':
+  //         break;
+  //       case 'svgSpriteTest':
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   })
+  // });
 };
